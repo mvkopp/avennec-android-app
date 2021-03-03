21c5578481deeaf05ce1c61934f08e2e195a4eeb
mvkopp <mael.avennec@hotmail.fr>
2021-03-03 23:13:17 +0100

    feat: add dao and database

---
+++ app/src/main/java/com/example/myawesomeapp/data/db/MealDao.java
@@ -0,0 +1,29 @@
+package com.example.myawesomeapp.data.db;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.myawesomeapp.data.entity.MealEntity;
+
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import io.reactivex.Single;
+
+@Dao
+public interface MealDao {
+
+    @Query("SELECT * from mealentity")
+    Flowable<List<MealEntity>> loadFavorites();
+
+    @Insert
+    public Completable addMealToFavorites(MealEntity mealEntity);
+
+    @Query("DELETE FROM mealentity WHERE id = :id")
+    public Completable deleteMealFromFavorites(String id);
+
+    @Query("SELECT id from mealentity")
+    Single<List<String>> getFavoriteList();
+}
---
+++ app/src/main/java/com/example/myawesomeapp/data/db/MealDataBase.java
@@ -0,0 +1,11 @@
+package com.example.myawesomeapp.data.db;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.example.myawesomeapp.data.entity.MealEntity;
+
+@Database(entities = {MealEntity.class}, version = 1)
+public abstract class MealDataBase extends RoomDatabase {
+    public abstract MealDao mealDao();
+}
