39f85d101522a98af82c91b28599d70b4b3ef9e3
mvkopp <mael.avennec@hotmail.fr>
2021-03-03 21:26:10 +0100

    refactor: change de type for get categories function

--- app/src/main/java/com/example/myawesomeapp/data/api/ApiService.java
+++ app/src/main/java/com/example/myawesomeapp/data/api/ApiService.java
@@ -1,12 +1,13 @@
 package com.example.myawesomeapp.data.api;
 
 import com.example.myawesomeapp.data.api.model.Meal;
+import com.example.myawesomeapp.data.api.model.MealsResponse;
 
-import retrofit2.Call;
+import io.reactivex.Single;
 import retrofit2.http.GET;
 
 public interface ApiService {
 
     @GET("categories.php")
-    Call<Meal> getCategories();
+    Single<MealsResponse> getCategories();
 }
