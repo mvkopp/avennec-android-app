b163b3198ebfc8e975362ddeb908fcaa8d00043e
mvkopp <mael.avennec@hotmail.fr>
2021-03-04 02:02:59 +0100

    feat: add item view model

---
+++ app/src/main/java/com/example/myawesomeapp/presentation/mealdisplay/meals/adapter/MealItemViewModel.java
@@ -0,0 +1,49 @@
+package com.example.myawesomeapp.presentation.mealdisplay.meals.adapter;
+
+public class MealItemViewModel {
+    private String id;
+    private String title;
+    private String description;
+    private String thumbnail;
+    private boolean isFavorite;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+}
